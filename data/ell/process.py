03ea284ed961eb4fb71bfde822331a7e2fad1055
161siegels <161siegels@github.com>
2023-02-19 19:18:08 -0800

    Data fixes for ell

---
+++ data/ell/process.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+REPLACE_DICT = {
+    "STUDENT_NAME": "Charlie",
+    "Generic_Name": "Charlie",
+    "LOCATION_NAME": "Chicago",
+    "Generic_Location": "Chicago",
+    "Generic_City": "Chicago",
+    "TEACHER_NAME": "Mr. Smith",
+    "Generic_School": "University of Chicago",
+}
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("data/ell/datasets/original_data.csv")
+    df = df['full_text'].replace(REPLACE_DICT)
+    df.to_csv("data/ell-data.csv", index=False)
--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 
 # https://www.kaggle.com/competitions/feedback-prize-english-language-learning
-ELL_DATA_DIR = 'data/ell_data.csv'
+ELL_DATA_DIR = 'data/ell-data.csv'
 FCE_DATA_DIR = "data/fce-data.csv"
 ETS_DATA_DIR = "data/ets-data.csv"
