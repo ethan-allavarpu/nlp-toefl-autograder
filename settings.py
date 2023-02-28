a26668e75db2882c834e2e6433bc491ad5a4ff5b
Ethan Allavarpu <allavarpuethan@gmail.com>
2023-02-27 19:38:45 -0800

    update settings

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 
 # https://www.kaggle.com/competitions/feedback-prize-english-language-learning
 ELL_DATA_DIR = 'data/ell-data.csv'
-FCE_DATA_DIR = "data/fce-data.csv"
+FCE_DATA_DIR = "data/fce-data-input-format.csv"
 ETS_DATA_DIR = "data/ets-data.csv"
 ICNALE_EDITED_DATA_DIR = "data/icnale-data-edited.csv"
 ICNALE_WRITTEN_DATA_DIR = "data/icnale-data-written.csv"
