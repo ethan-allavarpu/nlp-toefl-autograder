0ba7286c4111b650c43cccd1b5889d30d9327495
duncan-ross <duncanross.eng@gmail.com>
2023-03-05 17:31:31 -0800

    Breakout utils file for modularization.

--- run.py
+++ run.py
@@ -7,82 +7,23 @@ from modeling.model import BaseModel, ETSModel, HierarchicalModel
 from torch.utils.tensorboard import SummaryWriter
 from transformers import AutoTokenizer
 import random
-import argparse
 
 from modeling import trainer
 from data_loading.datasets import DefaultDataset, CombinedDataset
 from data_loading.dataloaders import get_data_loaders
-from settings import *
+import run_utils as utils
 
 torch.manual_seed(0)
-argp = argparse.ArgumentParser()
-argp.add_argument('function', help="Choose pretrain, finetune, or evaluate") #TODO: add behavior for pretrain and eval
-argp.add_argument("--model_type", type=str, default="base", required=False)
-argp.add_argument('--writing_params_path', type=str, help='Path to the writing params file', required=False)
-argp.add_argument('--reading_params_path', type=str, help='Path to the reading params file', required=False)
-argp.add_argument('--loss_path', type=str, help='Path to the output losses', default="losses.txt", required=False)
-argp.add_argument('--outputs_path', type=str, help='Path to the output predictions', default="predictions.txt", required=False)
-argp.add_argument('--tokenizer_name', type=str, help='Name of the tokenizer to use', default="distilbert-base-uncased", required=False)
-argp.add_argument('--dataset', type=str, help='Name of the dataset to use', default="ICNALE-EDITED", required=False)
-argp.add_argument("--ICNALE_output", type=str, help="Use 'categories' or 'overall' score", default="overall", required=False)
-argp.add_argument('--max_epochs', type=int, help='Number of epochs to train for', default=20, required=False)
-argp.add_argument('--learning_rate', type=float, help='Learning rate', default=2e-5, required=False)
-argp.add_argument('--lr_decay', type=str, help='Decay Learning Rate', default="False", required=False)
-args = argp.parse_args()
+args = utils.get_argparser().parse_args()
 
 # Save the device
 device = torch.cuda.current_device() if torch.cuda.is_available() else 'cpu'
 args.lr_decay = args.lr_decay == "True"
 
-# instantiate the tokenizer
-# tokenizer = AutoTokenizer.from_pretrained("ccdv/lsg-xlm-roberta-base-4096", trust_remote_code=True)
+# Instantiate the tokenizer and dataset
 tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name, trust_remote_code=True)
-# instantiate the dataset
-if args.model_type in ["base", "ets"]:
-    if args.dataset == "ELL":
-        dataset = DefaultDataset(file_path=ELL_DATA_DIR, input_col='full_text', target_cols=['cohesion', 'syntax',  'vocabulary',  'phraseology',  'grammar',  'conventions'], index_col='text_id', 
-                                tokenizer=tokenizer)
-    elif args.dataset == "ICNALE-EDITED":
-        if args.ICNALE_output == "overall":
-            dataset = DefaultDataset(file_path=ICNALE_EDITED_DATA_DIR, input_col='essay', target_cols=['Total 1 (%)'], index_col=None, 
-                                    tokenizer=tokenizer)
-        else:
-            dataset = DefaultDataset(file_path=ICNALE_EDITED_DATA_DIR, input_col='essay', target_cols=['Content (/12)', 'Organization (/12)',
-        'Vocabulary (/12)', 'Language Use (/12)', 'Mechanics (/12)'], index_col=None, 
-                                    tokenizer=tokenizer)
-    elif args.dataset == "ICNALE-WRITTEN": #TODO: lots of missing fields in this dataset, what imputations should we use?
-        dataset = DefaultDataset(file_path=ICNALE_WRITTEN_DATA_DIR, input_col='essay', target_cols=['Score'], index_col="sortkey", 
-                                tokenizer=tokenizer)
-    elif args.dataset == "FCE":
-        dataset = DefaultDataset(file_path=FCE_DATA_DIR, input_col='essay', target_cols=['overall_score'], 
-                                tokenizer=tokenizer)
-    elif args.dataset == "ETS":
-        dataset = DefaultDataset(file_path=ETS_DATA_DIR, input_col='response', target_cols=['score'], 
-                                tokenizer=tokenizer)
-    else:
-        raise ValueError("Invalid dataset name")
-elif args.model_type == "hierarchical":
-    if args.dataset == "ELL-ICNALE":
-        dataset = CombinedDataset(
-            file_path1=ELL_DATA_DIR, file_path2=ICNALE_EDITED_DATA_DIR, input_col1="full_text", input_col2="essay",
-            target_cols1=['cohesion', 'syntax',  'vocabulary',  'phraseology',  'grammar',  'conventions'],
-            target_cols2=['Total 1 (%)'], tokenizer=tokenizer)
-    elif args.dataset == "FCE":
-        dataset = DefaultDataset(file_path=FCE_DATA_DIR, input_col='essay', target_cols=['overall_score'], 
-                                tokenizer=tokenizer)
-    elif args.dataset == "ICNALE-EDITED":
-        if args.ICNALE_output == "overall":
-            dataset = DefaultDataset(file_path=ICNALE_EDITED_DATA_DIR, input_col='essay', target_cols=['Total 1 (%)'], index_col=None, 
-                                    tokenizer=tokenizer)
-        else:
-            dataset = DefaultDataset(file_path=ICNALE_EDITED_DATA_DIR, input_col='essay', target_cols=['Content (/12)', 'Organization (/12)',
-        'Vocabulary (/12)', 'Language Use (/12)', 'Mechanics (/12)'], index_col=None, 
-                                    tokenizer=tokenizer)
-    elif args.dataset == "ETS":
-        dataset = DefaultDataset(file_path=ETS_DATA_DIR, input_col='response', target_cols=['score'], 
-                                tokenizer=tokenizer)
-    else:
-        raise ValueError("Invalid dataset name")
+dataset = utils.get_dataset(args, tokenizer)
+
 
 if args.function == 'pretrain':
     writer = SummaryWriter(log_dir='expt/')
@@ -90,6 +31,7 @@ if args.function == 'pretrain':
     train_config = trainer.TrainerConfig(max_epochs=args.max_epochs, 
             learning_rate=args.learning_rate, lr_decay=args.lr_decay,
             num_workers=4, writer=writer, ckpt_path='expt/params.pt')
+
     if args.model_type == "base":
         train_dl, val_dl, test_dl = get_data_loaders(dataset, val_size=0.2, test_size=0, batch_size=16, val_batch_size=1,
         test_batch_size=1, num_workers=0)
@@ -182,10 +124,7 @@ elif args.function == 'evaluate':
         predictions.append((model(x)[0].mean().item(), y[0].mean().item()))
         torch.cuda.empty_cache()
 
-    with open(args.outputs_path, 'w') as f:
-        for pred in predictions:
-            f.write(f"{pred[0]},{pred[1]}\n")
+    utils.write_predictions(args.outputs_path, predictions)
     
-
 else:
     print("Invalid function name. Choose pretrain, finetune, or evaluate")
