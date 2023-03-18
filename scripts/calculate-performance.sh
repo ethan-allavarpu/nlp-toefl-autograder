cc4d8af9db8101312f6719dc0c4b81cfcf06f8b4
duncan-ross <duncanross.eng@gmail.com>
2023-03-18 12:38:21 -0700

    Repo cleanup

--- scripts/calculate-performance.sh
+++ scripts/calculate-performance.sh
@@ -2,15 +2,15 @@
 
 python src/evaluation.py "predictions/fce/new-icnale-conv.fce.predictions" fce base 10 "images/output_icnale_conv_10.png"
 
-python src/evaluation.py "PARAMS AND PREDICTIONS FROM BEFORE/icnale-baseline.fce.predictions" fce base 10 "images/output_icnale_10.png"
+python src/evaluation.py "predictions/legacy_predictions/icnale-baseline.fce.predictions" fce base 10 "images/output_icnale_10.png"
 
-python src/evaluation.py "PARAMS AND PREDICTIONS FROM BEFORE/ell-baseline.fce.predictions" fce base 10 "images/output_ell_10.png"
+python src/evaluation.py "predictions/legacy_predictions/ell-baseline.fce.predictions" fce base 10 "images/output_ell_10.png"
 
-python src/evaluation.py "PARAMS AND PREDICTIONS FROM BEFORE/icnale-baseline-categories.fce.predictions" fce base 10 "images/output_icnale_categories_10.png"
+python src/evaluation.py "predictions/legacy_predictions/icnale-baseline-categories.fce.predictions" fce base 10 "images/output_icnale_categories_10.png"
 
-python src/evaluation.py "PARAMS AND PREDICTIONS FROM BEFORE/double-pretrain.fce.predictions" fce base 10 "images/output_double_pretrain_10.png"
+python src/evaluation.py "predictions/legacy_predictions/double-pretrain.fce.predictions" fce base 10 "images/output_double_pretrain_10.png"
 
-python src/evaluation.py "PARAMS AND PREDICTIONS FROM BEFORE/hierarchical-baseline.fce.predictions" fce base 10 "images/output_hierarchical_10.png"
+python src/evaluation.py "predictions/legacy_predictions/hierarchical-baseline.fce.predictions" fce base 10 "images/output_hierarchical_10.png"
 
 # Trial 1 final params
 python src/evaluation.py icnale-baseline-more-drop.fce.predictions fce base 10 "images/output_icnale_more_drop_10.png"
@@ -48,15 +48,15 @@ python src/evaluation-ensemble.py 3 ell-baseline.fce.predictions ets-pretrain.fc
 
 # AGAIN BUT FOR 5 BUCKETS
 
-python src/evaluation.py "PARAMS AND PREDICTIONS FROM BEFORE/icnale-baseline.fce.predictions" fce base 5 "images/output_icnale_5.png"
+python src/evaluation.py "predictions/legacy_predictions/icnale-baseline.fce.predictions" fce base 5 "images/output_icnale_5.png"
 
-python src/evaluation.py "PARAMS AND PREDICTIONS FROM BEFORE/ell-baseline.fce.predictions" fce base 5 "images/output_ell_5.png"
+python src/evaluation.py "predictions/legacy_predictions/ell-baseline.fce.predictions" fce base 5 "images/output_ell_5.png"
 
-python src/evaluation.py "PARAMS AND PREDICTIONS FROM BEFORE/icnale-baseline-categories.fce.predictions" fce base 5 "images/output_icnale_categories_5.png"
+python src/evaluation.py "predictions/legacy_predictions/icnale-baseline-categories.fce.predictions" fce base 5 "images/output_icnale_categories_5.png"
 
-python src/evaluation.py "PARAMS AND PREDICTIONS FROM BEFORE/double-pretrain.fce.predictions" fce base 5 "images/output_double_pretrain_5.png"
+python src/evaluation.py "predictions/legacy_predictions/double-pretrain.fce.predictions" fce base 5 "images/output_double_pretrain_5.png"
 
-python src/evaluation.py "PARAMS AND PREDICTIONS FROM BEFORE/hierarchical-baseline.fce.predictions" fce base 5 "images/output_hierarchical_5.png"
+python src/evaluation.py "predictions/legacy_predictions/hierarchical-baseline.fce.predictions" fce base 5 "images/output_hierarchical_5.png"
 
 # Trial 1 final params
 python src/evaluation.py icnale-baseline-more-drop.fce.predictions fce base 5 "images/output_icnale_more_drop_5.png"
@@ -91,6 +91,6 @@ python src/evaluation-ensemble.py 2 ets-pretrain.fce.predictions ell-baseline.fc
 python src/evaluation-ensemble.py 3 ell-baseline.fce.predictions ets-pretrain.fce.predictions icnale-baseline.predictions 1 2 2 fce 5 "images/output_ets_ell_icnale_5.png"
 
 
-python src/evaluation-ensemble.py 2 "PARAMS AND PREDICTIONS FROM BEFORE/hierarchical-baseline.fce.predictions" icnale-baseline-more-drop.fce.predictions 1 2 fce 10 "images/output_iter_icnale_10.png" "images/output_iter_icnale_10-hist.png" "Ensemble Model Confusion Matrix"
+python src/evaluation-ensemble.py 2 "predictions/legacy_predictions/hierarchical-baseline.fce.predictions" icnale-baseline-more-drop.fce.predictions 1 2 fce 10 "images/output_iter_icnale_10.png" "images/output_iter_icnale_10-hist.png" "Ensemble Model Confusion Matrix"
 
-python src/evaluation-ensemble.py 2 "PARAMS AND PREDICTIONS FROM BEFORE/hierarchical-baseline.fce.predictions" icnale-baseline-more-drop.fce.predictions 1 2 fce 5 "images/output_iter_icnale_5.png"
+python src/evaluation-ensemble.py 2 "predictions/legacy_predictions/hierarchical-baseline.fce.predictions" icnale-baseline-more-drop.fce.predictions 1 2 fce 5 "images/output_iter_icnale_5.png"
